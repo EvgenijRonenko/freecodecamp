4855bc4e56e3c45933e64df211a25c42a0ed7198
EvgenijRonenko <evgenij.ronenko@gmail.com>
2022-11-26 17:46:07 +0100

    30 done

---
+++ 03.js
@@ -0,0 +1,63 @@
+021
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+022
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -=1;
+
+023
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+024
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+025
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+026
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+027
+const myStr = 'This is the start. ' + 'This is the end.'; // Change this line
+
+028
+let myStr = 'This is the first sentence. ';
+myStr += 'This is the second sentence.';
+
+029
+// Only change code below this line
+const myName = "Evgenij";
+const myStr = 'My name is' + myName + ', and I am well!';
+
+030
+// Change code below this line
+const someAdjective = "How";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
