af66146b1eb6316d3359c5965a141e057add702b
EvgenijRonenko <evgenij.ronenko@gmail.com>
2022-11-26 20:37:30 +0100

    40 done

---
+++ 04.js
@@ -0,0 +1,68 @@
+031
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+032
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+033
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr= "Hello World"; // Change this line
+// Only change code above this line
+
+034
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+let thirdLetterOfLastName = lastName[2]; // Change this line
+
+035
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+036
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+037
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myNoun + ' ' + myAdjective + ' ' + myVerb + ' ' + myAdverb; // Change this line
+// Only change code above this line
+
+038
+// Only change code below this line
+const myArray = ['1', 2];
+
+039
+// Only change code below this line
+const myArray = [[1,2]];
+
+040
+const myArray = [50, 60, 70];
+let myData = myArray[0];
+
+
