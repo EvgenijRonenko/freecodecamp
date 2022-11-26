75d1063db854ad38dec325435331a6880b107c41
EvgenijRonenko <evgenij.ronenko@gmail.com>
2022-11-26 16:01:57 +0100

    10st done

--- 01.js
+++ 01.js
@@ -1,5 +1,73 @@
+01
 /**
  * Comment for test
  */
 
 // Comment for
+
+02
+var myName;
+
+03
+// Setup
+var a;
+
+// Only change code below this line
+
+a = 7;
+
+04
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+b = 7;
+b = a;
+
+05
+var a = 9;
+
+06
+var myFirstName = 'Evgenij';
+var myLastName = 'Ronenko';
+
+
+
+07
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a = 5;
+b = 10;
+c = 'I am a';
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+08
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+09
+let catName = "Oliver";
+let catSound = "Meow!";
+
+10
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
