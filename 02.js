2111f5a0629b5a189c813c8486720ea3dfb43455
EvgenijRonenko <evgenij.ronenko@gmail.com>
2022-11-26 16:57:21 +0100

    20 done

---
+++ 02.js
@@ -0,0 +1,39 @@
+011
+const sum = 10 + 10;
+
+012
+const difference = 45 - 33;
+
+013
+const product = 8 * 10;
+
+014
+const quotient = 66 / 33;
+
+015
+let myVar = 87;
+
+// Only change code below this line
+myVar = ++myVar;
+
+016
+let myVar = 11;
+
+// Only change code below this line
+myVar = --myVar;
+
+017
+const ourDecimal = 5.7;
+
+// Only change code below this line
+
+let myDecimal = 3.14;
+
+018
+const product = 2.0 * 2.5;
+
+019
+const quotient = 4.4 / 2.0; // Change this line
+
+020
+const remainder = 11 % 3;
